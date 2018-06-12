c5bccad6cec187fb25ed10eddf46a5306f00324c
Chasen Arey <ccarey@ncsu.edu>
2018-06-12 13:20:03 -0600

    problem2

--- higher-order-func.js
+++ higher-order-func.js
@@ -6,19 +6,44 @@ created hello and goodbye funcs
 
 */
 
-function hello(){
-    console.log('hello world');
-}
-function goodbye(){
-    console.log('goodbye world');
-}
+// function hello(){
+//     console.log('hello world');
+// }
+// function goodbye(){
+//     console.log('goodbye world');
+// }
 
-function repeat(fn, n){
-    let i = 1;
-    while (i <= n){
-        fn();
-        i++;
+// function repeat(fn, n){
+//     let i = 1;
+//     while (i <= n){
+//         fn();
+//         i++;
+//     }
+// }
+
+// repeat(hello, 7);
+
+// DO NOT EDIT BETWEEN THESE LINES ----->
+// Return only names that begin with 'R'
+const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
+
+const filteredNames = filter(myNames, function(name) {
+    // This is a "predicate function" - it's a function that only returns a boolean
+    return name[0] === 'R';
+});
+
+console.log(filteredNames) // => ['Rich', 'Ray']
+// <---- DO NOT EDIT BETWEEN THESE LINES
+
+// TASK: DEFINE YOUR FILTER FUNCTION BELOW:
+function filter(arr, fn) {
+    let newArray = [];
+    for (let i=0; i<arr.length; i++){
+        if (fn(arr[i]) === true){
+            newArray.push(arr[i]);
+        }
+    return newArray;
     }
 }
 
-repeat(hello, 7);
+filter(myNames, filteredNames);
