4bba0b05e3e6c86e46313216e6e9dd48b602d972
Chasen Arey <ccarey@ncsu.edu>
2018-06-12 11:02:53 -0600

    average function ans

--- loop_array-drills.js
+++ loop_array-drills.js
@@ -1,28 +1,32 @@
-<<<<<<< HEAD
-function max(numbers){
-    let curMax = numbers[0];
-    let i = 1;
-    while (i < numbers.length){
-        if (numbers[i] > curMax){
-            curMax = numbers[i];
-        }
-        i++;
-    }
-    return curMax;
-}
+
+// function max(numbers){
+//     let curMax = numbers[0];
+//     let i = 1;
+//     while (i < numbers.length){
+//         if (numbers[i] > curMax){
+//             curMax = numbers[i];
+//         }
+//         i++;
+//     }
+//     return curMax;
+// }
 
 
-function min(numbers){
-    let curMin = numbers[0];
-    let i = 1;
-    while (i < numbers.length){
-        if (numbers[i] < curMin){
-            curMin = numbers[i];
-        }
-        i++;
-    }
-    return curMin;
+// function min(numbers){
+//     let curMin = numbers[0];
+//     let i = 1;
+//     while (i < numbers.length){
+//         if (numbers[i] < curMin){
+//             curMin = numbers[i];
+//         }
+//         i++;
+//     }
+//     return curMin;
+// }
+
+
+function average(arr) {
+    let totalSum = 0
+    arr.forEach(num => totalSum += num)
+    return totalSum/arr.length     
 }
-=======
-hey
->>>>>>> e0f8564bfb38880d7881e9b46e40f08679fad964
