bbc0374f2dd2081771afca727f068b04195433e2
Chasen Arey <ccarey@ncsu.edu>
2018-06-12 14:41:14 -0600

    turtle ans

--- higher-order-func.js
+++ higher-order-func.js
@@ -26,52 +26,66 @@ created hello and goodbye funcs
 
 
 
-/*
-// DO NOT EDIT BETWEEN THESE LINES ----->
-// Return only names that begin with 'R'
-const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
 
-const filteredNames = filter(myNames, function(name) {
-    // This is a "predicate function" - it's a function that only returns a boolean
-    return name[0] === 'R';
-});
+// // DO NOT EDIT BETWEEN THESE LINES ----->
+// // Return only names that begin with 'R'
+// const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
 
-console.log(filteredNames) // => ['Rich', 'Ray']
-// <---- DO NOT EDIT BETWEEN THESE LINES
+// const filteredNames = filter(myNames, function(name) {
+//     // This is a "predicate function" - it's a function that only returns a boolean
+//     return name[0] === 'R';
+// });
 
-// TASK: DEFINE YOUR FILTER FUNCTION BELOW:
-function filter(arr, fn) {
-    let newArray = [];
-    for (let i=0; i<arr.length; i++){
-        if (fn(arr[i]) === true){
-            newArray.push(arr[i]);
-        }
-    return newArray;
-    }
-}
+// console.log(filteredNames) // => ['Rich', 'Ray']
+// // <---- DO NOT EDIT BETWEEN THESE LINES
 
-filter(myNames, filteredNames);
+// // TASK: DEFINE YOUR FILTER FUNCTION BELOW:
+// function filter(arr, fn) {
+//     let newArray = [];
+//     for (let i=0; i<arr.length; i++){
+//         if (fn(arr[i]) === true){
+//             newArray.push(arr[i]);
+//         }
+//     return newArray;
+//     }
+// }
 
-*/
+// const biggerThanSix = filter(myNames, function(name){return name.length<6});
+
+
 //created a warningCounter
 //return a function that logs a danger message
 //returning another log message
 //where to increase counter??????
 
 
-function hazardWarningCreator(typeOfWarning){
-    let warningCounter = 0;
-    return function(location){
-        warningCounter++;
-        console.log(`DANGER! There is a ${typeOfWarning} hazard at ${location}!`);
-        console.log(`The ${typeOfWarning} hazard alert has triggered ${warningCounter} time(s) today!`);
-    }
-}
 
-const rocksWarning = hazardWarningCreator("Rocks on the Road");
-const windWarning = hazardWarningCreator("It's too windy!");
-const lightningWarning = hazardWarningCreator("look out for lightning!");
 
-rocksWarning("California");
-windWarning("Kansas");
+// function hazardWarningCreator(typeOfWarning){
+//     let warningCounter = 0;
+//     return function(location){
+//         warningCounter++;
+//         console.log(`DANGER! There is a ${typeOfWarning} hazard at ${location}!`);
+//         console.log(`The ${typeOfWarning} hazard alert has triggered ${warningCounter} time(s) today!`);
+//     }
+// }
+
+// const rocksWarning = hazardWarningCreator("Rocks on the Road");
+// const windWarning = hazardWarningCreator("It's too windy!");
+// const lightningWarning = hazardWarningCreator("look out for lightning!");
+
+// rocksWarning("California");
+// windWarning("Kansas");
+// rocksWarning("California");
+
+
+turtlesMovements = [[0, 0], [0, 5], [-1, -3], [-3, 1], [2, -4], [3, 2]];
+
+let turtlePostiveNum = turtlesMovements.filter( num => num[0]>=0 && num[1]>=0);
+console.log(turtlePostiveNum);
+let turtleSteps = turtlePostiveNum.map(num => num[0]+num[1]);
+console.log(turtleSteps);
+turtleSteps.forEach(move => console.log(`Turtle took ${move} steps`));
+
+
 
